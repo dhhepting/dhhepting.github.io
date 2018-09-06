857a62f196726d279a669f70c2312e737602bc20
Daryl Hepting <heptingd@acm.org>
2018-09-06 16:16:12 -0600

    removed #CC anchor for now

--- script/teaching-rss.py
+++ script/teaching-rss.py
@@ -123,7 +123,7 @@ if (local_now <= latest):
         rss_file.write("layout: rss\n")
         rss_file.write("---\n")
         # header for feed
-        rss_file.write("<?xml version=\"1.0\"?>")
+        rss_file.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>")
         rss_file.write("<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n")
         rss_file.write(st+"<channel>\n")
         rss_file.write(st+st+"<title>" + fc_title + "</title>\n")
