ca80749e06dd7fa1a9e305be775cda0981f1cdaa
Daryl Hepting <heptingd@acm.org>
2019-06-10 16:46:17 -0600

    fixes to meeting code

--- script/off_meetings.py
+++ script/off_meetings.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys, os, datetime, subprocess
+import sys, os, datetime, subprocess, errno
 from datetime import datetime
 from subprocess import Popen, PIPE
 
@@ -21,7 +21,7 @@ if (len(reldir) != 2):
 
 off_index_html = os.path.join(os.path.abspath(SITE_DIR+HTML_ROOT),sys.argv[1],"index.html")
 grep_html = Popen(("grep", "html", off_index_html), stdout=PIPE)
-grep_href = Popen(("grep", "href"), stdin=grep_html.stdout, stdout=PIPE)
+grep_href = Popen(("grep", "href=\"meetings"), stdin=grep_html.stdout, stdout=PIPE)
 outs, errs = grep_href.communicate()
 utfout = (outs.decode("utf-8")).split('\n')
 
@@ -31,7 +31,24 @@ for line in utfout:
         nbr_meetings += 1
 m = 0
 mtgdatafile = os.path.join(os.path.abspath(SITE_DIR+DATA_ROOT),reldir[0]+"-"+reldir[1]+".csv")
+mtgdatafile = mtgdatafile.replace("+","_")
 print(mtgdatafile)
+mdirstr = str(SITE_DIR + MD_ROOT + sys.argv[1] + "/meetings")
+try:
+    os.makedirs(mdirstr)
+except OSError as e:
+    if e.errno == errno.EEXIST and os.path.isdir(mdirstr):
+        pass
+    else:
+        raise
+with open(SITE_DIR + MD_ROOT + sys.argv[1] + "/meetings/index.md","w") as mtgidx:
+    mtgidx.write("---\n")
+    mtgidx.write("title: " + reldir[0] + " (" + reldir[1] + ") Meetings\n")
+    mtgidx.write("breadcrumb: Meetings\n")
+    mtgidx.write("layout: bg-image\n")
+    mtgidx.write("---\n")
+    mtgidx.write("#" + reldir[0] + " (" + reldir[1] + ") Meetings\n")
+    mtgidx.write("{% include index-dir.html %}\n")
 with open(mtgdatafile,"w") as mtgdata:
     mtgdata.write("meeting,file\n")
     for line in utfout:
@@ -41,17 +58,23 @@ with open(mtgdatafile,"w") as mtgdata:
             mtg_fname = (words[1].split('.'))[0]
             mtgdata.write(str(m).zfill(2) + "," + mtg_fname.split('/')[1] + ".html\n")
             datestr = mtg_fname.split('-',1)
-            mtg_dt = datetime.strptime(datestr[1],"%d-%b-%y")
-            mfilestr = SITE_DIR + MD_ROOT + sys.argv[1] + "/" + mtg_fname + ".md"
-            with open(mfilestr,"w") as mtgfile:
-                mtgfile.write("---\n")
-                mtgfile.write("title: " + reldir[0] + " (" + reldir[1] + ") Mtg " + str(m) + "\n")
-                mtgfile.write("breadcrumb: " + str(m) + " (" + mtg_dt.strftime('%d-%b-%y') + ")\n")
-                mtgfile.write("mtg_nbr: " + str(m) + "\n")
-                mtgfile.write("total_meet: " + str(nbr_meetings) + "\n")
-                mtgfile.write("mtg_date: " + mtg_dt.strftime('%d-%b-%y') + "\n")
-                mtgfile.write("layout: bg-image\n")
-                mtgfile.write("---\n")
-                mtgfile.write("{% include mtg-pagination.html %}\n")
-                mtgfile.write("<h1 class=\"text-center\">{{ page.mtg_date }}</h1>\n<hr />\n")
-                mtgfile.write("{% include meeting-media.html mtg_media=off_med mtg=page.mtg_nbr %}\n")
+            #print(mtg_fname)
+            try:
+                mtg_dt = datetime.strptime(datestr[1],"%d-%b-%y")
+                #print (mtg_dt)
+                mfilestr = SITE_DIR + MD_ROOT + sys.argv[1] + "/" + mtg_fname + ".md"
+                #print(mfilestr)
+                with open(mfilestr,"w") as mtgfile:
+                    mtgfile.write("---\n")
+                    mtgfile.write("title: " + reldir[0] + " (" + reldir[1] + ") Mtg " + str(m) + "\n")
+                    mtgfile.write("breadcrumb: " + str(m) + " (" + mtg_dt.strftime('%d-%b-%y') + ")\n")
+                    mtgfile.write("mtg_nbr: " + str(m) + "\n")
+                    mtgfile.write("total_meet: " + str(nbr_meetings) + "\n")
+                    mtgfile.write("mtg_date: " + mtg_dt.strftime('%d-%b-%y') + "\n")
+                    mtgfile.write("layout: bg-image\n")
+                    mtgfile.write("---\n")
+                    mtgfile.write("{% include mtg-pagination.html %}\n")
+                    mtgfile.write("<h1 class=\"text-center\">{{ page.mtg_date }}</h1>\n<hr />\n")
+                    mtgfile.write("{% include meeting-media.html mtg_media=off_med mtg=page.mtg_nbr %}\n")
+            except:
+                pass
