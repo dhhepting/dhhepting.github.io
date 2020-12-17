acd15615288196ac8852a698c3c02af95d92140b
Daryl Hepting <heptingd@acm.org>
2020-12-17 15:10:16 -0600

    chat files update

--- script/groups-of-3-4-eval.py
+++ script/groups-of-3-4-eval.py
@@ -7,46 +7,50 @@ from pathlib import Path
 from random import randrange
 
 
-# arguments to this script:
-# - the absolute path to the website's local root directory
+# argument to this script:
+# - path to group file
 if (len(sys.argv) != 2):
   print (sys.argv[0],"must be invoked with \"<path-to-group-file>\"")
   sys.exit()
 
-# get site directory, make sure it ends with "/"
 groupfilepath = sys.argv[1]
-og = []
+oldgrp = []
 nstu = 0
 with open(groupfilepath,'r') as gf:
     for line in gf:
         l = line.split(',')
-        og.append(l)
+        oldgrp.append(l)
         nstu = nstu + int(l[len(l)-1])
-ng = {}
-grp = int(nstu/3)
-for i in range(grp):
-    ng[i+1] = []
+newgrp = {}
+nngrps = int(nstu/3)
+for i in range(nngrps):
+    newgrp[i] = []
+#print(newgrp)
 nk = 1
 stu = 0
-for l in og:
+for l in oldgrp:
     for m in range(1,len(l)-1):
         mgrp = 0
-        nk = (nk + 1 + int(grp/3) + randrange(3)) % (grp + 1)
-        if (nk == 0):
-            nk = 1
-        # fg = '(' + l[0] + ')'
-        fg = '(' + l[m] + ')'
-        for q in range(grp):
-            if (len(ng[nk]) < 3 and fg not in ng[nk]):
-                ng[nk].append(fg)
-                stu = stu + 1
-                break
-            else:
-                nk = (nk + 1) % (grp + 1)
-                if (nk == 0):
-                    nk = 1
-        print (stu,nstu,l[m])
+        nk = (nk + 1 + (m * int(nngrps/3))) % nngrps
+        fg = (l[0].replace(' ','-'),l[m].strip())
+        for q in range(nngrps):
+            nq = (nk + q) % nngrps
+            if len(newgrp[nq]) < 3:
+                if any(fg[0] in s for s in newgrp[nq]):
+                    pass
+                    #print(stu,nq,'fail:',fg[0],'--',newgrp[nq])
+                    # and fg[0] not in newgrp[nk]):
+                else:
+                    newgrp[nq].append(fg)
+                    #print(stu,nq,'success:',fg[0],'--',newgrp[nq])
+                    stu = stu + 1
+                    break
+            # else:
+            #     nk = (nk + 1) % (nngrps + 1)
+            #     if (nk == 0):
+            #         nk = 1
+        #print (stu,l[m].strip())
 
-for x in ng:
-    print(x,ng[x])
+for x in newgrp:
+    print(x,newgrp[x])
 sys.exit()
