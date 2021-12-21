2dc304933afc680e0a21b14277f56fd65fcd3a95
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-12-21 20:35:39 +0000

    Deploy dhhepting/dhhepting.github.io to github.com/dhhepting/dhhepting.github.io.git:gh-pages

---
+++ script/responses-by-student.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import sys, os, errno
+from datetime import datetime, timedelta
+import csv
+import yaml
+from yaml import SafeDumper
+
+SafeDumper.add_representer(
+    type(None),
+    lambda dumper, value: dumper.represent_scalar(u'tag:yaml.org,2002:null', '')
+  )
+
+
+SITE_DIR = "/Users/hepting/Sites/dhhepting.github.io/"
+DL_DIR = "/Users/hepting/Downloads/"
+MD_ROOT = "teaching/"
+HTML_ROOT = "_site/teaching/"
+DATA_ROOT = "_data/teaching/"
+
+# Make sure that script is executed properly: i.e. CS-428+828/201830
+if (len(sys.argv) != 2):
+	print(sys.argv[0], 'must be invoked with <course>/<semester>')
+	sys.exit()
+
+reldir = (sys.argv[1]).split('/')
+if (len(reldir) != 2):
+	print(sys.argv[0], 'must be invoked with <course>/<semester>')
+	sys.exit()
+
+jcrs_id = reldir[0].replace("+","_")
+joff_id = jcrs_id + '-' + reldir[1]
+RESP_DIR = DL_DIR + joff_id + '/'
+sss = {}
+for root, dirs, files in os.walk(RESP_DIR):
+    for file in files:
+        if file.endswith(".csv"):
+            rfile = os.path.join(root, file)
+            mk = file.split(".")[0]
+            with open(rfile, newline='') as rrrfile:
+                reader = csv.DictReader(rrrfile)
+                for row in reader:
+                    #print(row["Email address"])
+                    emi = row["Email address"].split("@")[0]
+                    #print(emi)
+                    if (emi not in sss):
+                        sss[emi] = {}
+                    else:
+                        sss[emi][mk] = row["(response) What is your response to today's meeting?"]
+for k in sss:
+    if len(sss[k]) > 0:
+        print (k, len(sss[k]))
+        #print (sss[k])
+        for r in sss[k]:
+            #print (r, len(sss[k]))
+            print (r,sss[k][r])
+        print("++++++++++++++++")
+"""
+        if (row['(description) Which description best suits your response?'] ==
+            'the most important thing that I learned'):
+            #print('important')
+            if 'important' not in d:
+                d['important'] = []
+            d['important'].append(e)
+        if (row['(description) Which description best suits your response?'] ==
+            'the most difficult thing for me to understand'):
+            if 'difficult' not in d:
+                d['difficult'] = []
+            d['difficult'].append(e)
+        if (row['(description) Which description best suits your response?'] ==
+            'the thing about which I would most like to know more'):
+            if 'know-more' not in d:
+                d['know-more'] = []
+            d['know-more'].append(e)
+try:
+    with open(respfile, 'w') as yaml_file:
+        yaml.safe_dump(d, yaml_file, default_flow_style=False)
+except:
+    print ('exceptions')
+"""
---
+++ script/summary-by-student.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+
+import sys, os, subprocess
+from fileinput import FileInput
+import os.path
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import webvtt
+
+import yaml
+from yaml import SafeDumper
+
+SafeDumper.add_representer(
+    type(None),
+    lambda dumper, value: dumper.represent_scalar(u'tag:yaml.org,2002:null', '')
+  )
+
+SITE_DIR = "/Users/hepting/Sites/dhhepting.github.io/"
+DL_DIR = "/Users/hepting/Downloads/"
+MD_ROOT = "teaching/"
+HTML_ROOT = "_site/teaching/"
+DATA_ROOT = "_data/teaching/"
+
+# Make sure that script is executed properly: i.e. CS-428+828/201830
+if (len(sys.argv) != 2):
+	print(sys.argv[0], 'must be invoked with <course>/<semester>')
+	sys.exit()
+
+reldir = (sys.argv[1]).split('/')
+if (len(reldir) != 2):
+	print(sys.argv[0], 'must be invoked with <course>/<semester>')
+	sys.exit()
+
+jcrs_id = reldir[0].replace("+","_")
+joff_id = jcrs_id + '-' + reldir[1]
+SUMM_DIR = DL_DIR + joff_id + '/'
+sss = {}
+for root, dirs, files in os.walk(SUMM_DIR):
+    for file in files:
+        if file.endswith(".vtt"):
+            sfile = os.path.join(root, file)
+            mk = file.split(".")[0]
+            ospkr = ''
+            for caption in webvtt.read(sfile):
+                txtseg = caption.text.split(':')
+                tsl = len(txtseg)
+                if (tsl == 1):
+                    spkr = 'Unknown'
+                    msg = txtseg[0]
+                if (tsl == 2):
+                    spkr = txtseg[0]
+                    msg = txtseg[1]
+                if (spkr != ospkr):
+                    if (ospkr != ''):
+                        snamekey = ospkr.strip()
+                        #print(snamekey)
+                        if snamekey not in sss:
+                            sss[snamekey] = 1
+                        else:
+                            sss[snamekey] += 1
+                    ospkr = spkr
+        if file.endswith(".txt"):
+            sfile = os.path.join(root, file)
+            mk = file.split(".")[0]
+            with open(sfile,'r') as ctf:
+                for line in ctf:
+                    npline = line.split('\t')
+                    if (len(npline) > 2):
+                        sname = npline[1][:-1]
+                        msg = npline[2]
+                        snamekey = sname.strip()
+                        if snamekey not in sss:
+                            sss[snamekey] = 1
+                        else:
+                            sss[snamekey] += 1
+for k in sss:
+    print(str(sss[k]) + '\t' + str(k))
+"""d ={}
+# try opening summary file for meeting
+jcrs_id = reldir[0].replace("+","_")
+
+try:
+    os.makedirs(summdir)
+except OSError as e:
+    # path already exists
+    pass
+summfile = summdir + '/' + sys.argv[2].zfill(2) + '.yml'
+joff_id = jcrs_id + '-' + reldir[1]
+mtg = sys.argv[2]
+try:
+    with open(summfile, 'x') as yaml_file:
+        d['offering'] = {}
+        d['offering']['id'] = joff_id
+        yaml.safe_dump(d, yaml_file, default_flow_style=False)
+except:
+    print(sys.argv[0],': ', summfile, 'exists')
+    with open(summfile, 'r') as yaml_file:
+        d = yaml.safe_load(yaml_file)
+
+try:
+    with open(planfile, 'r') as plyaml_file:
+        p = yaml.safe_load(plyaml_file)
+except:
+    pass
+
+print ('Processing outline for meeting:')
+if 'today' in p[int(sys.argv[2])]:
+    for te in p[int(sys.argv[2])]['today']:
+        if (te not in d):
+            d[te] = []
+            #d[te].append(te)
+            d[te].append(p[int(sys.argv[2])]['today'][te])
+elif 'outline' in p[int(sys.argv[2])]:
+    if ('Outline' not in d):
+        d['Outline'] = []
+        d['Outline'].append(p[int(sys.argv[2])]['outline'])
+    else:
+        print ('\t--> Outline already present.')
+
+ng = {}
+seqnbr = 0
+
+
+VTT_SRC = DL_DIR + joff_id + '/' + sys.argv[2].zfill(2) + '.vtt'
+#print(VTT_SRC)
+print ('Processing audio transcript for meeting:')
+if (os.path.isfile(VTT_SRC)):
+    if ('Zoom-Audio-Transcript' not in d):
+########
+        d['Zoom-Audio-Transcript'] = []
+        ospkr = ''
+        msgstr = ''
+########
+        for caption in webvtt.read(VTT_SRC):
+            #print("Caption:",caption)
+            txtseg = caption.text.split(':')
+            tsl = len(txtseg)
+            if (tsl == 1):
+                spkr = 'Unknown'
+                msg = txtseg[0]
+            if (tsl == 2):
+                spkr = txtseg[0]
+                msg = txtseg[1]
+            #print('\tmsg from caption:',msg)
+            #print(spkr,ospkr)
+            if (spkr != ospkr):
+                if (ospkr != ''):
+                    snamekey = ospkr.strip()
+                    if snamekey not in ng:
+                        ng[snamekey] = seqnbr
+                        seqnbr += 1
+                    if snamekey == 'Daryl Hepting' or snamekey == 'Daryl (heptingd)':
+                        person_id = 'DHH'
+                    elif snamekey == 'Unknown':
+                        person_id = '???'
+                    else:
+                        person_id = 'S'+str(ng[snamekey]).zfill(2)
+                    e = {}
+                    e['desc'] = msgstr.strip()
+                    #print("desc:",e['desc'])
+                    e['persid'] = person_id
+                    d['Zoom-Audio-Transcript'].append(e)
+                    msgstr = ''
+                ospkr = spkr
+            msgstr += msg
+            #print('\t\trunning msg str:',msgstr)
+########
+        e = {}
+        e['desc'] = msgstr.strip()
+        #print("desc:",e['desc'])
+        e['persid'] = ''
+        d['Zoom-Audio-Transcript'].append(e)
+    else:
+        print ('\t--> Zoom-Audio-Transcript already present.')
+
+
+CHAT_SRC = DL_DIR + joff_id + '/' + sys.argv[2].zfill(2) + '.txt'
+#print(CHAT_SRC)
+print ('Processing chat transcript for meeting:')
+
+if (os.path.isfile(CHAT_SRC)):
+    if ('Zoom-Chat-Transcript' not in d):
+        d['Zoom-Chat-Transcript'] = []
+        with open(CHAT_SRC,'r') as ctf:
+            for line in ctf:
+                npline = line.split('\t')
+                if (len(npline) > 2):
+                    sname = npline[1][:-1]
+                    msg = npline[2]
+                    snamekey = sname.strip()
+                    if snamekey not in ng:
+                        ng[snamekey] = seqnbr
+                        seqnbr += 1
+                    if snamekey == 'Daryl Hepting' or snamekey == 'Daryl (heptingd)':
+                        person_id = 'DHH'
+                    else:
+                        person_id = 'S'+str(ng[snamekey]).zfill(2)
+                    e = {}
+                    e['desc'] = msg.strip()
+                    e['persid'] = person_id
+                    d['Zoom-Chat-Transcript'].append(e)
+    else:
+        print ('\t--> Zoom-Chat-Transcript already present.')
+with open(summfile, 'w') as yaml_file:
+    yaml.safe_dump(d, yaml_file, default_flow_style=False)
+
+"""
