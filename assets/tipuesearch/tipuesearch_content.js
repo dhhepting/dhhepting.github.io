2d1cc171c39134e1220218fc1ef9db9efdc116c8
dhhepting <dhhepting@users.noreply.github.com>
2026-08-10 16:41:21 +0000

    deploy: 61fdad0c881722be1bfe3a6c2692e359526e87f8

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -3135,7 +3135,7 @@ var tipuesearch = {"pages": [{
     "url": "/~hepting/about/biography.html"
   },{
     "title": "Schedule",
-    "text": "Schedule for Current Semester (“current” as of 2026-08-10 01:45:42 -0600) The current semester is: undefined Important Dates First day of term: Last day of classes: Last day of term:",
+    "text": "Schedule for Current Semester (“current” as of 2026-08-10 10:40:33 -0600) The current semester is: undefined Important Dates First day of term: Last day of classes: Last day of term:",
     "tags": "",
     "url": "/~hepting/teaching/schedule/copy-index.html"
   },{
